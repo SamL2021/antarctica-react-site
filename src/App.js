516e58dbad7b2c8a07998f26e290c7c83af6e1c3
Samantha Lippiatt <sgd@live.com.au>
2022-06-20 11:36:17 +1000

    Add content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./penguin-logo.png";
 import "./App.css";
+import Footer from "./Components/Footer";
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Antarctica React Site Coming Soon</p>
+                <Footer></Footer>
             </header>
         </div>
     );
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+        <footer className={styles["footer"]}>
+            Samantha Lippiatt © All Rights Reserved
+        </footer>
+    );
+};
+
+export default Footer;
