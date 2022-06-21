b743c7848e43bb2c3c78a3359180cb5cbca323db
Samantha Lippiatt <sgd@live.com.au>
2022-06-21 15:41:53 +1000

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <div class="footer-links"></div>
+            <section class="social-media">
+                <div class="social-media-wrap">
+                    <div class="footer-logo">
+                        <Link to="/" className="social-logo">
+                            BEYOND ADVENTURES
+                            {/* <i class="fab fa-typo3" /> */}
+                        </Link>
+                    </div>
+                    <small class="website-rights">S.LIPPIATT © 2022</small>
+                    <div class="social-icons">
+                        <Link
+                            class="social-icon-link instagram"
+                            to="/"
+                            target="_blank"
+                            aria-label="Instagram"
+                        >
+                            <i class="fab fa-instagram" />
+                        </Link>
+                        <Link
+                            class="social-icon-link youtube"
+                            to="/"
+                            target="_blank"
+                            aria-label="Youtube"
+                        >
+                            <i class="fab fa-youtube" />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/pages/Journey.js
+++ src/components/pages/Journey.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../../App.css";
+// import Footer from "../Footer";
 
 export default function Journey() {
-    return <h1 className="journey">The Journey</h1>;
+    return (
+        <>
+            <h1 className="the-journey">The Journey</h1>
+            {/* <Footer /> */}
+        </>
+    );
 }
